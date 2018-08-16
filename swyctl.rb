5146cb84137782b727580e0374667858e46d407f
admin <vp@swifty.cloud>
2018-08-16 18:02:11 +0300

    update

--- swyctl.rb
+++ swyctl.rb
@@ -1,7 +1,7 @@
 class Swyctl < Formula
   desc "A command line tool to retrieve local weather"
   homepage "https://github.com/swiftycloud/swyctl"
-  url "https://github.com/swiftycloud/swyctl/blob/master/archive/swyctl-1.0.0.tar.gz"
+  url "https://raw.githubusercontent.com/swiftycloud/swyctl/master/archive/swyctl-1.0.0.tar.gz"
   sha256 "00ef246e6293faebcf1eea56545bd56d68396567ed070180d003c4d365e3b12e"
   #version "1.0.0"
 
